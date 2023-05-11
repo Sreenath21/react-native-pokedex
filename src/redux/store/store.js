6f24a6f2467376c49016172f09771d0c259453e7
Sreenath P <srep2@WKMZTA60B680.global.publicisgroupe.net>
2023-05-11 10:51:37 +0530

    updated store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,23 +1,3 @@
-// import {configureStore} from '@reduxjs/toolkit';
-// import pokeIdsReducer from '../features/pokeIdsSlice';
-// import pokeDetailsReducer from '../features/pokeDetailsSlice';
-
-// export const store = configureStore({
-//   reducer: {
-//     // pokemons: pokemonsReducer,
-//     pokemonIds: pokeIdsReducer,
-//     pokemonDetails: pokeDetailsReducer,
-//   },
-//   middleware: getDefaultMiddleware =>
-//     getDefaultMiddleware({
-//       // serializableCheck: {
-//       //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-//       // },
-//       serializableCheck: false,
-//       immutableCheck: false,
-//     }),
-// });
-
 import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
